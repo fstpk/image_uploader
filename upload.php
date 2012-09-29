d95dd41abc2ab3461f9ce119aebcf95a402dd885
fastpoke <fastpoke@fastpoke.org>
2012-09-29 21:02:58 +0700

    modified:   add error status message

--- upload.php
+++ upload.php
@@ -2,6 +2,26 @@
  //e.g. http://server.tld/
  $url = ; 
 
+ $file = $_FILES['file'];
+ $error_text = true;
+ define("UPLOAD_ERR_EMPTY",5);
+   if($file['size'] == 0 && $file['error'] == 0){
+     $file['error'] = 5;
+   }
+  $upload_errors = array(
+    UPLOAD_ERR_OK        => "No errors.",
+    UPLOAD_ERR_INI_SIZE    => "Larger than upload_max_filesize.",
+    UPLOAD_ERR_FORM_SIZE    => "Larger than form MAX_FILE_SIZE.",
+    UPLOAD_ERR_PARTIAL    => "Partial upload.",
+    UPLOAD_ERR_NO_FILE        => "No file.",
+    UPLOAD_ERR_NO_TMP_DIR    => "No temporary directory.",
+    UPLOAD_ERR_CANT_WRITE    => "Can't write to disk.",
+    UPLOAD_ERR_EXTENSION     => "File upload stopped by extension.",
+    UPLOAD_ERR_EMPTY        => "File is empty."
+  );
+ $err = ($error_text) ? $upload_errors[$file['error']] : $file['error'] ;
+
+
  $blacklist = array(".php", ".phtml", ".php3", ".php4", ".pl", ".py", ".sh", ".exe");
  foreach ($blacklist as $item) {
   if(preg_match("/$item\$/i", $_FILES['userfile']['name'])) {
@@ -132,6 +152,8 @@ echo "
            </div>
            <div class=\"message\">
             So much failed :(
+            <br>
+            Error: <span class=\"bold red\">$err</span>
            </div>
           </div>
           <div class=\"back\"><a href=\"$url\" target=\"_self\">back</a></div>
