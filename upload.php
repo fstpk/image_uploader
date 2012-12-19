0f330e2a4eac0ff99385ac862119ad36b0402829
fastpoke <fastpoke@fastpoke.org>
2012-12-19 18:16:29 +0700

    yet another small fix

--- upload.php
+++ upload.php
@@ -9,15 +9,15 @@
      $file['error'] = 5;
    }
   $upload_errors = array(
-    UPLOAD_ERR_OK        => "No errors.",
-    UPLOAD_ERR_INI_SIZE    => "Larger than upload_max_filesize.",
-    UPLOAD_ERR_FORM_SIZE    => "Larger than form MAX_FILE_SIZE.",
-    UPLOAD_ERR_PARTIAL    => "Partial upload.",
-    UPLOAD_ERR_NO_FILE        => "No file.",
-    UPLOAD_ERR_NO_TMP_DIR    => "No temporary directory.",
-    UPLOAD_ERR_CANT_WRITE    => "Can't write to disk.",
-    UPLOAD_ERR_EXTENSION     => "File upload stopped by extension.",
-    UPLOAD_ERR_EMPTY        => "File is empty."
+    UPLOAD_ERR_OK		=> "No errors.",
+    UPLOAD_ERR_INI_SIZE		=> "Larger than upload_max_filesize.",
+    UPLOAD_ERR_FORM_SIZE	=> "Larger than form MAX_FILE_SIZE.",
+    UPLOAD_ERR_PARTIAL		=> "Partial upload.",
+    UPLOAD_ERR_NO_FILE		=> "No file.",
+    UPLOAD_ERR_NO_TMP_DIR	=> "No temporary directory.",
+    UPLOAD_ERR_CANT_WRITE	=> "Can't write to disk.",
+    UPLOAD_ERR_EXTENSION	=> "File upload stopped by extension.",
+    UPLOAD_ERR_EMPTY		=> "File is empty."
   );
  $err = ($error_text) ? $upload_errors[$file['error']] : $file['error'] ;
 
@@ -96,12 +96,13 @@
      $time = date("d.m.y");
      $filesize = filesize("$uploaddir$md5.$ext");
      $size = (int)($filesize/1024);
-     /*$loadtime = (int)($filesize/20480);*/
+     $type = $imageinfo['mime'];
      $loadtime = rand(10, 100);
+     /*$loadtime = (int)($filesize/20480);*/
      /* if($delay>10) 
 	$delay = 10;
      sleep($delay);*/
-     $type = $imageinfo['mime'];
+
 
 echo "
 	 <!DOCTYPE html>
